4e47e084dba852f1038a640b343ede6e20ecc359
Aleksey <shatalov_alekse@mail.ru>
2022-10-18 23:50:01 +0700

    все задачи по лекции

---
+++ Lecture/8 алгоритм сортировки min и max/Program.cs
@@ -0,0 +1,33 @@
+﻿// алгоритм сортировки мин и макс (например 5,9,6,3,7 а станет 3,5,6,7,9)
+
+int[] arr = {1,5,4,3,2,6,7,1,1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+       Console.Write($"{array[i]}"); 
+    }
+    Console.WriteLine();
+}
+
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+       int minPosition = i;
+     for (int j = i + 1; j < array.Length; j++)
+     {
+         if (array[j] < array[minPosition]) minPosition = j;
+     }
+       int temporary = array[i];
+       array[i] = array[minPosition];
+       array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
